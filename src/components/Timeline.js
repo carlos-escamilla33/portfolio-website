b5283042e80d3a50dd8bb78bd930b8c1509842e1
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-02-05 02:58:33 -0800

    finished timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { DesktopComputerIcon } from '@heroicons/react/solid';
+import { FlagIcon } from '@heroicons/react/solid';
+import { achievements } from '../data';
 
 const Timeline = () => {
     return (
-        <div className='container mx-auto px-6'>
-            <div>
-                <DesktopComputerIcon className="w-24 h-24 rounded-full shadow-md border-4 border-white text-white"/>
-            </div>
-            <div className='relative p-2'>
-                <div className="absolute inset-0 left-10 h-4 w-4 transform rotate-45 bg-white" aria-hidden="true"></div>
-                <div className='bg-white p-6 rounded-md shadow-md'>
-                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
+        <section id="timeline">
+            <div className="container px-5 py-10 mx-auto">
+                <div className="text-center mb-20">
+                    <FlagIcon className="w-10 inline-block mb-4" />
+                    <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+                        My Journey into Programming
+                    </h1>
                 </div>
+                <ol className="relative border-l border-gray-200 dark:border-gray-700 container px-5 py-10 mx-auto">
+                    {
+                        achievements.map((achievements) => (
+                            <li className="mb-10 ml-5" key={achievements.date}>
+                                <div className="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
+                                <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{achievements.date}</time>
+                                <h3 className="text-lg font-semibold text-gray-600 dark:text-white">{achievements.subtitle}</h3>
+                                <p className="mb-4 text-base font-normal text-gray-700 dark:text-gray-400">{achievements.description}</p>
+                            </li>
+                        ))
+                    }
+                </ol>
             </div>
-        </div>
+        </section>
     )
 }
 
