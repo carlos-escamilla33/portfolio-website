b20b51e71e3984f29aa5679a3555db5b3f76dc37
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-02-06 16:32:55 -0800

    fixed timeline

--- src/data.js
+++ src/data.js
@@ -50,19 +50,9 @@ export const skills = [
 
 export const achievements = [
     {
-        date: "November 2021",
-        subtitle: "Graduated from FullStack Academy",
-        description: "I learned about Javascript technologies during my time at FullStack Academy. I built multiple fullstack applications and learned about coding best practices along with data structures and algorithms. I gained so many great relationships along the way that I will always value."
-    },
-    {
-        date: "May 2021",
-        subtitle: "Enrolled in FullStack Academy",
-        description: "With my internship ending in Janurary of 2021 I looked for more knowledge. I learned about coding bootcamps and decided to take the untraditional route into software development."
-    },
-    {
-        date: "November 2020",
-        subtitle: "First web development internship (Remote)",
-        description: "After working extremely hard to build on my Python skills I landed my first coding internship. There I learned about Django and form validation in a team of three with two other interns"
+        date: "January 2020",
+        subtitle: "I took my first programming class.",
+        description: "My first programming class was an intro to python course that dealt with data structures and algorithms at San Jose State Univeristy. After taking that class I realized I had a passion for coding."
     },
     {
         date: "June 2020",
@@ -70,8 +60,18 @@ export const achievements = [
         description: "I used Python and Jupyter Notebook to build a foodie bot. The application let you pick from a menu and narrowed down food/resturant choices to help you pick a place to eat faster."
     },
     {
-        date: "January 2020",
-        subtitle: "I took my first programming class.",
-        description: "My first programming class was an intro to python course that dealt with data structures and algorithms at San Jose State Univeristy. After taking that class I realized I had a passion for coding."
+        date: "November 2020",
+        subtitle: "First web development internship (Remote)",
+        description: "After working extremely hard to build on my Python skills I landed my first coding internship. There I learned about Django and form validation in a team of three with two other interns"
     },
+    {
+        date: "May 2021",
+        subtitle: "Enrolled in FullStack Academy",
+        description: "With my internship ending in Janurary of 2021 I looked for more knowledge. I learned about coding bootcamps and decided to take the untraditional route into software development."
+    },
+    {
+        date: "November 2021",
+        subtitle: "Graduated from FullStack Academy",
+        description: "I learned about Javascript technologies during my time at FullStack Academy. I built multiple fullstack applications and learned about coding best practices along with data structures and algorithms. I gained so many great relationships along the way that I will always value."
+    }
 ]
